9bef4b7079bdbdafe5189c502ce4ca1b17067724
Samuel <samuelkaye92@gmail.com>
2026-05-09 17:54:10 +0200

    Day 1 - Variables and Types

---
+++ Week 1/day-1-variables/Program.cs
@@ -0,0 +1,7 @@
+﻿var name = "Sammy";
+int age = 23;
+bool dev = true;
+string city = "Johannesburg";
+
+
+Console.WriteLine($"My name is {name}, I am {age} years old, and it is {dev} that I am a developer. I live in {city}.");
