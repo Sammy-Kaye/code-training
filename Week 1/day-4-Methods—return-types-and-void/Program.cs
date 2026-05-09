61e08df960a28ccfb0bcf1f50ad2d3646a6028b3
Samuel <samuelkaye92@gmail.com>
2026-05-09 19:55:16 +0200

    day-4-Methods—return-types-and-void

---
+++ Week 1/day-4-Methods—return-types-and-void/Program.cs
@@ -0,0 +1,21 @@
+﻿void PrintDeveloperStatus()
+{
+    bool isDeveloper = true;
+     if (isDeveloper == true)
+    {
+        Console.WriteLine("Sammy is a developer: True");
+    }
+}
+
+bool getDeveloperStatus()
+{
+    bool isDeveloper = true;
+    return isDeveloper;
+}
+
+PrintDeveloperStatus();
+
+if (getDeveloperStatus() == true)
+{
+    Console.WriteLine("Developer status: True");
+}
