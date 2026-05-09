7bc8d94de51c9227ede659a8c051a1b5b99ff57e
Samuel <samuelkaye92@gmail.com>
2026-05-09 23:26:22 +0200

    day-6 BOSS FIGHT

---
+++ Week 1/BOSS FIGHT/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("What is your name?");
+string? name = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+string? input = Console.ReadLine();
+int age = int.Parse(input ?? "0");
+
+string GetGreeting(string? name, int age)
+{
+   string result = age >= 18  ? $"Hello, {name}! You are an adult." : $"Hello, {name}! You are a minor.";
+   return result;
+   
+}
+
+string output = GetGreeting(name, age);
+Console.WriteLine(output);
