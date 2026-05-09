b958a7fde7fef0efb273e583518438826a5f947e
Samuel <samuelkaye92@gmail.com>
2026-05-09 18:41:44 +0200

    Day 2 - if else

---
+++ Week 1/day-2-if-else/Program.cs
@@ -0,0 +1,14 @@
+﻿string colour = "";
+
+if (colour == "red")
+{
+    Console.WriteLine("Stop.");
+}
+else if (colour == "green")
+{
+    Console.WriteLine("Go.");
+}
+else
+{
+    Console.WriteLine("Slow down.");
+}
