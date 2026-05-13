3df7fa10287ae667c6bab3d6b2814a0cbefb60a5
Sammy.K <samuel.k@cyberfactor.net>
2026-05-13 17:51:08 +0200

    SP-2: POST, PUT, DELETE endpoints for colours CRUD

--- Special Lessons/SP-1-Web-API/Program.cs
+++ Special Lessons/SP-1-Web-API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -12,6 +14,24 @@ app.MapGet("/colours", () =>
     return colours;
 });
 
+app.MapPost("/colours", ([FromBody] string colour) =>
+{
+    colours.Add(colour);
+    return $"You added the colour: {colour}";
+});
+
+app.MapPut("/colours", ([FromBody] UpdateColourDto request) =>
+{
+    colours[colours.IndexOf(request.OldColour)] = request.NewColour;
+    return $"You changed {request.OldColour} to {request.NewColour}";
+});
+
+app.MapDelete("/colours", ([FromBody] string colour) =>
+{
+    colours.Remove(colour);
+    return $"You deleted: {colour}";
+});
+
 var Name = "Sam";
 var JobTitle = "Developer";
 
@@ -21,3 +41,5 @@ app.MapGet("/greeting", () =>
 });
 
 app.Run();
+
+record UpdateColourDto(string OldColour, string NewColour);
