a36ae474535d42c3fa0f0557b7293e31b8a667c9
Sammy.K <samuel.k@cyberfactor.net>
2026-05-13 19:18:16 +0200

    day-8-while-loops

---
+++ Week 2/day-2-while-loops/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please enter Password");
+
+string? userInput = Console.ReadLine();
+string? password = "letmein";
+
+while (userInput != password)
+{
+    Console.WriteLine("Wrong Password, try again");
+    userInput = Console.ReadLine();
+}
+
+Console.WriteLine("Access Granted");
